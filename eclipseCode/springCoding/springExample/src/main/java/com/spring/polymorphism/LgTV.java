3f352bfd274cbc162d8a8fb138ddaddc021bd307
kwonjinbeom <78678666+kwonjinbeom@users.noreply.github.com>
2023-02-08 15:19:37 +0900

    new file

---
+++ eclipseCode/springCoding/springExample/src/main/java/com/spring/polymorphism/LgTV.java
@@ -0,0 +1,19 @@
+package com.spring.polymorphism;
+
+public class LgTV implements TV{
+	public LgTV() {
+		System.out.println("===>LgTV(1) 객체 생성");
+	}
+	public void powerOn() {
+		System.out.println("LgTV---전원 켠다.");
+	}
+	public void powerOff() {
+		System.out.println("LgTV---전원 끈다.");
+	}
+	public void volumeUp() {
+		System.out.println("LgTV---소리를 올린다.");
+	}
+	public void volumeDown() {
+		System.out.println("LgTV---소리를 내린다.");
+	}
+}
